67d50680119395ff493cb3068d43d27fdd19f59d
Matthias Schlögl <m.schloegl@gmail.com>
2018-01-18 15:25:21 +0100

    remove not needed context processor

--- context_processors/custom_context_processors.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.contenttypes.models import ContentType
-
-def add_entities(request):
-    res = {'entities_list': [x.name for x in ContentType.objects.filter(app_label='entities')],
-            'request': request}
-    return res
